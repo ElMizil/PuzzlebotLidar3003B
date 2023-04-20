0f279e6498f27468e947958e387d72749b92e387
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2023-04-19 19:15:52 -0600

    Update puzzlebotkinematic_model.py

--- MR_chlgs/puzzlebot_sim/scripts/week1/puzzlebotkinematic_model.py
+++ MR_chlgs/puzzlebot_sim/scripts/week1/puzzlebotkinematic_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# Broadcaster para enviar la posicion de un robot y visualizarla en Rviz
+# Simulador cinematico para Puzzlebot con RVIZ
 
 # Librerias
 import rospy
@@ -10,6 +10,7 @@ from geometry_msgs.msg import PoseStamped, Twist, TransformStamped, Vector3
 from tf.transformations import quaternion_from_euler
 from std_msgs.msg import Float32, Empty
 
+#Declaramos la clase del puzzlebot
 class puzzlebot_cinematic:
    def __init__(self):
       # Iniciamos las velocidades y la posicion del robot
@@ -29,23 +30,26 @@ class puzzlebot_cinematic:
       self.path_pub = rospy.Publisher('/path',Path, queue_size=10)
       rospy.Subscriber('/reset', Empty, self.reset)
 
-   # Obtener velocidades
+   # Metodo para asignar la velocidad obtenida de /cmd_vel
+   # Nota: Solo necesitamos la velocidad lineal en x y la angular en z
    def setVel(self,msg):
       self.vel_angular = msg.angular.z
       self.vel_linear = msg.linear.x
 
-   # Calcular la posicion
+   # Metodo para obtener la posicion y orientacion del robot
    def getPuzzleVel(self):
       self.x += self.vel_linear*math.cos(self.angle)/self.freq
       self.y += self.vel_linear*math.sin(self.angle)/self.freq
       self.angle += math.radians(self.vel_angular)
       self.getPuzzlePos()
    
-   # Asignar la posicion al topico de pose_sim
+   # Metodo para obtener la pose del robot para publicar en /pose_sim
    def getPuzzlePos(self):
       pose = PoseStamped()
+      # Empezamos poniendo la posicion
       pose.pose.position.x = self.x
       pose.pose.position.y = self.y
+      # Luego generamos los quaterniones
       q = quaternion_from_euler(0,0,self.angle)
 
       pose.pose.orientation.x = q[0]
@@ -54,24 +58,26 @@ class puzzlebot_cinematic:
       pose.pose.orientation.w = q[3]
 
       pose.header.frame_id = "base_link"
+      # Publicamos el pose y mandamos la transformada
       self.broadcastTransform(pose.pose.orientation)
       self.pose_pub.publish(pose)
       print(pose)
 
-   # Enviar la transformada usando un broadcaster
+   # Mandamos la transformada usando un broadcaster
    def broadcastTransform(self, orientation):
         # Inicializar broadcaster
         br = TransformBroadcaster()
         t = TransformStamped()
-        # Fill the transform with the position and orientations
+        # Llenamos la transformada con la posicion y las orientaciones
         t.header.stamp = rospy.Time.now()
         # Nombres de los frames
         t.header.frame_id = "base_link"
         t.child_frame_id = "chassis"
         t.transform.translation = Vector3(self.x, self.y, 0)
         t.transform.rotation = orientation
-        # Enviar la transformada
+        # Mandamos la transformada
         br.sendTransform(t)
+        # Generamos el camino
         m = Path()
         m.header = t.header
         pose = PoseStamped()
@@ -83,8 +89,9 @@ class puzzlebot_cinematic:
         m.poses.append(pose)
         self.path_pub.publish(m)
    
-   # Calcular la velocidad en cada rueda
+   # Metodo para obtener las velocidades de las llantas
    def getWheelVel(self):
+      # Calculamos la velocidad de cada rueda y lo publicamos
       self.wl.publish((self.vel_linear - self.vel_angular)/self.r)
       self.wr.publish((self.vel_linear + self.vel_angular)/self.r)      
 
